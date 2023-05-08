2cbdf451756dd52ca4c34cd2bb7c92e4cb64beb8
aidan <67598470+livxy@users.noreply.github.com>
2023-05-08 17:38:51 -0400

    Refactored `unswear.py` with proper documentation + keywords. Added more specific type hinting for better maintenance.

--- unswear.py
+++ unswear.py
@@ -1,15 +1,35 @@
-from pynput.keyboard import Listener, Key, Controller, KeyCode
 import csv
+from typing import Dict
+
+from pynput.keyboard import Controller, Key, KeyCode, Listener
 
 
 class Recorder:
-    buffer = ""
+    """
+    A class to record and evaluate typed words.
 
-    def __init__(self, word_pairs: dict[str, str]):
+    Attributes:
+        word_pairs (Dict[str, str]): A dictionary of words to be replaced and their corresponding replacement.
+        buffer (str): A buffer to store the current word being typed.
+        keyboard (Controller): A controller to handle keyboard input.
+    """
+
+    def __init__(self, pairs: Dict[str, str]):
+        """
+        Initializes the Recorder with the given word pairs.
+
+        :param pairs: A dictionary of words to be replaced and their corresponding replacement.
+        """
         self.keyboard = Controller()
-        self.word_pairs = word_pairs
+        self.word_pairs = pairs
+        self.buffer = ""
 
     def on_press(self, key):
+        """
+        Handles key press events.
+
+        :param key: The key that was pressed.
+        """
         if key == Key.space:
             self.evaluate_word()
             self.buffer = ""
@@ -21,29 +41,46 @@ class Recorder:
             self.buffer += key.char
 
     def handle_backspace(self):
+        """
+        Handles backspace events by removing the last character from the buffer.
+        """
         self.buffer = self.buffer[:-1]
 
     def on_release(self, key):
+        """
+        Handles key release events.
+
+        :param key: The key that was released.
+        """
         pass
 
     def evaluate_word(self):
+        """
+        Evaluates the current word in the buffer and replaces it if necessary.
+        """
         if self.buffer in self.word_pairs:
             self.delete_word(len(self.buffer))
-            self.keyboard.type(self.word_pairs[self.buffer] + " ")
+            self.keyboard.type(f"{self.word_pairs[self.buffer]} ")
 
     def delete_word(self, length):
-        for i in range(length + 1):
+        """
+        Deletes the current word by sending backspace events.
+
+        :param length: The length of the word to be deleted.
+        """
+        for _ in range(length + 1):
             self.keyboard.press(Key.backspace)
             self.keyboard.release(Key.backspace)
 
+
 if __name__ == "__main__":
-    pairs: dict[str, str] = {}
+    word_pairs: Dict[str, str] = {}
     with open('replacements.csv', newline='') as f:
         reader = csv.reader(f)
         for item in reader:
-            pairs[item[0]] = item[1]
+            word_pairs[item[0]] = item[1]
 
-    recorder = Recorder(pairs)
+    recorder = Recorder(word_pairs)
 
     with Listener(on_press=recorder.on_press, on_release=recorder.on_release) as listener:
         listener.join()
